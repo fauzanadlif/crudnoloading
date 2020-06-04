bcca8bd973d4af9543c9de6288c00f3752e24740
fauzanadlif <55770811+fauzanadlif@users.noreply.github.com>
2020-06-04 23:03:15 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = "localhost";
+$db_database = "db_siswa";
+$db_username = "root";
+$db_password = "";
+$db_port = "3306";
+
+// membuat koneksi
+$conn = new mysqli($db_host, $db_username, $db_password, $db_database, $db_port);
+
+// cek koneksi
+if ($conn->connect_error) {
+    die('Connection failed:' . $conn->connect_error);
+}
